256ac851caff9b54b3d0fe7e7c325c1151fb0ebd
Valdir Silva <valdir.ti@gmail.com>
2019-10-05 10:40:12 -0300

    Configurando o tempo de conexão ativa

--- www/painel/config.php
+++ www/painel/config.php
@@ -24,3 +24,20 @@ try {
 	echo "ERRO: ".$e->getMessage();
 	exit;
 }
+
+//Delimitando tempo de sessão
+$timeout = 600; // Tempo da sessao em segundos
+// Verifica se existe o parametro timeout
+if(isset($_SESSION['timeout'])) {
+    // Calcula o tempo que ja se passou desde a cricao da sessao
+    $duracao = time() - (int) $_SESSION['timeout'];
+
+  	// Verifica se ja expirou o tempo da sessao
+    if($duracao > $timeout) {
+        // Destroi a sessao e cria uma nova
+        session_destroy();
+        session_start();
+    }
+}
+// Atualiza o timeout.
+$_SESSION['timeout'] = time();
